18a4f7a7f66d09c2957bdf7007c9bb5ecd92d442
maaxg <maxgps5@hotmail.com>
2023-03-18 21:16:54 -0300

    feat: signup and signin on localstorage

---
+++ js/authentication.js
@@ -0,0 +1,65 @@
+const signUpForm = document.getElementById("signup-form");
+const signInForm = document.getElementById("signin-form");
+const signUpButton = document.getElementById("signup-button");
+const signInButton = document.getElementById("signin-button");
+
+const formValidation = (entries) => {
+  let valid = true;
+  for (let pair of entries.entries()) {
+    if (!Boolean(pair[1])) valid = false;
+    //console.log(pair[0] + ": " + pair[1]);
+  }
+  if (!valid) {
+    alert("Preencha todos os campos");
+  }
+  return valid;
+};
+
+const signup = (ev) => {
+  ev.preventDefault();
+  const formData = new FormData(ev.target);
+  // iterate through entries...
+  const isValid = formValidation(formData);
+
+  if (isValid) {
+    let users = [];
+    const savedUsers = JSON.parse(localStorage.getItem("users"));
+    if (savedUsers) {
+      users = savedUsers;
+    }
+    users.push(Object.fromEntries(formData));
+
+    localStorage.setItem("users", JSON.stringify(users));
+    console.log(users);
+    window.location = "http://127.0.0.1:5500/html/disciplines.html";
+    //console.log(Object.fromEntries(formData));
+  }
+  // ...or output as an object
+};
+
+const signin = (ev) => {
+  ev.preventDefault();
+  const formData = new FormData(ev.target);
+  const signIn = Object.fromEntries(formData);
+
+  const users = JSON.parse(localStorage.getItem("users"));
+
+  if (users) {
+    const founded = users.find(
+      (item) => item.email === signIn.email && item.password === signIn.password
+    );
+    if (founded) {
+      localStorage.setItem("user", JSON.stringify(founded));
+      window.location = "http://127.0.0.1:5500/html/disciplines.html";
+    } else alert("Usuário não encontrado");
+  }
+  /*   if (u) {
+   
+  } */
+};
+
+const onLoad = () => {};
+onLoad();
+
+signUpForm?.addEventListener("submit", signup, false);
+signInForm?.addEventListener("submit", signin, false);
